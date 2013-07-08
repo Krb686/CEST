50fb8d6bb114178d05118143546f718068b957c3
Kevin Briggs <krb686@gmail.com>
2013-07-08 14:04:42 -0400

    -Setup local map example @GMU -Added sample OSM building (rappahannock) -Added full screen control

--- app.js
+++ app.js
@@ -3,33 +3,78 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express     =   require('express')
+  , routes      =   require('./routes')
+  , user        =   require('./routes/user')
+  , http        =   require('http')
+  , path        =   require('path')
+  , io          =   require('socket.io');
 
-var app = express();
+  
+var users = [];
+var socketList = {};
+  
+function configureServer()
+{
+    
+    //Create app
+    var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+    app.configure(function(){
+      app.set('port', process.env.PORT || 3000);
+      app.set('views', __dirname + '/views');
+      app.set('view options', {layout:false});
+      app.engine('html', require('ejs').renderFile);
+      app.use(express.favicon());
+      app.use(express.logger('dev'));
+      app.use(express.bodyParser());
+      app.use(express.methodOverride());
+      app.use(app.router);
+      app.use(express.static(path.join(__dirname, 'public')));
+    });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+    app.configure('development', function(){
+      app.use(express.errorHandler());
+    });
+    
+    app.get('/', function(req, res){
+        res.render('index.html');
+    });
+    
+    startServer(app);
+}
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+function startServer(app)
+{
+    var server = http.createServer(app);
+    var port = app.get('port');
+    
+    server.listen(port, function(){
+        console.log("Express server listening on port " + port);
+        io = io.listen(server);
+        io.set('log level', 1);
+      
+        io.sockets.on('connection', connectSocket);
+    });
+}
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+function connectSocket(socket){
+    
+    
+    console.log(socket.id + '...connected!');
+    socketList[socket.id] = socket;
+    console.log('  # Users: ' + Object.keys(socketList).length);
+ 
+    //disconnect event
+    socket.on('disconnect', function(){
+        console.log(socket.id + '...disconnected.');
+        delete socketList[socket.id];
+        console.log('  # Users: ' + Object.keys(socketList).length);
+    });
+    
+    socket.on('simulation', function(data){
+       console.log(data); 
+    });
+}
+
+configureServer();
