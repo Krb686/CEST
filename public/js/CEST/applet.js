4fb70b99f2cdd2b9c0af8398ef5c7f08b9ab42f9
Kevin Briggs <krb686@gmail.com>
2013-08-06 16:11:55 -0400

    -more updates, code cleanup, popup improvement

--- public/js/CEST/applet.js
+++ public/js/CEST/applet.js
@@ -40,8 +40,6 @@ CEST.prototype = {
     loadDevices: function(){
     
         var deviceCount = 5;
-    
-        //var el = document.createElement('div');
         var popupHTML = "<div class='detail-popup-info'>" + 
                             "<ul>" + 
                                 "<li>" + 
@@ -56,7 +54,40 @@ CEST.prototype = {
                                 "</li>" + 
                             "</ul>" + 
                             "<div id='tabDevice'>" + 
-                                "<p>Device</p>" + 
+                                "<table class='popupTable'>" + 
+                                    "<tr>" + 
+                                        "<td>" + 
+                                            "<ul>" + 
+                                                "<li><b>Cisco E2100L</li>" + 
+                                            "</ul" + 
+                                        "</td>" + 
+                                        "<td>" + 
+                                            "<ul>" + 
+                                            "</ul" + 
+                                        "</td>" +
+                                    "</tr>" + 
+                                    "<tr>" + 
+                                        "<td>" + 
+                                            "<ul>" + 
+                                                "<li><b>ID: 134de92c178</li>" + 
+                                            "</ul" + 
+                                        "</td>" + 
+                                        "<td class='tdImg'>" + 
+                                            "<img class='routerImage' src='/images/cisco_e2100l.gif' style='height:128px;width:128px;text-align:center;'></img>" + 
+                                        "</td>" +
+                                    "</tr>" + 
+                                    "<tr>" + 
+                                        "<td>" + 
+                                            "<ul>" + 
+                                                "<li><b>Enterprise Hall</li>" + 
+                                            "</ul" + 
+                                        "</td>" + 
+                                        "<td>" + 
+                                            "<ul>" + 
+                                            "</ul" + 
+                                        "</td>" +
+                                    "</tr>" + 
+                                "</table>" + 
                             "</div>" + 
                             "<div id='tabInfo'>" + 
                                 "<p>Info</p>" + 
@@ -81,8 +112,26 @@ CEST.prototype = {
             }
         };
         
+        var device2 = {
+            'name': 'Cisco Wireless Router',
+            'type': 'Router',
+            'id' : '134de92c178',
+            'mfc' : 'Cisco',
+            'model' : 'E1000',
+            'properties': {
+                'ip' : '192.168.1.12',
+                'mac' : '22-DC-C4-92-4D-BE',
+                'status' : 'Good',
+                'location' : {
+                    'lat' : 38.82905,
+                    'lng' : -77.30632
+                }
+            }
+        };
+        
        
         this.devices.push(device1);
+        this.devices.push(device2);
         
         
         this.markers = new L.MarkerClusterGroup();
@@ -90,14 +139,23 @@ CEST.prototype = {
             var device = this.devices[i];
             
             if(device.type == 'Router'){
+                
+                
                 var routerIcon = L.AwesomeMarkers.icon({
                     icon: 'icon-signal',
                     color: 'darkred'
                 });
                 
                 var newMarker = new L.Marker([device.properties.location.lat, device.properties.location.lng], {icon:routerIcon});
-                newMarker.bindPopup(popupHTML);
+                var popup = L.popup({maxWidth:385}).setContent(popupHTML);
+                newMarker.bindPopup(popup);
                 this.markers.addLayer(newMarker);
+                
+                
+                /*
+                var marker = L.marker([38.83, -77.3]).addTo(this.map);
+                marker.bindPopup(popupHTML);
+                */
                 
             }
         }
@@ -109,7 +167,8 @@ CEST.prototype = {
     },
     
     loadPlugins: function(){
-    
+        
+        
         //Fullscreen control
         L.control.fullscreen({
           position: 'topleft',
@@ -131,12 +190,25 @@ CEST.prototype = {
         
         //OSM
         var osmBuildings = new OSMBuildings(this.map).setData(buildingData);
+        console.log(buildingData);
         var featuresLayer = L.geoJson(buildingData);
         featuresLayer.setStyle({stroke:false, fill:false});
         
         //leaflet-search
-        this.map.addControl( new L.Control.Search({layer: featuresLayer, propertyName: "name", animateLocation:false, circleLocation:false}) );
-    }
+        var searchControl = new L.Control.Search({layer: featuresLayer, propertyName: "name", animateLocation:false, circleLocation:false});
+        this.map.addControl(searchControl);
+        //this.sampleSearchControl();
+    },
+    sampleSearchControl: function(){
+        var data = buildingData;
+        
+        var featuresLayer = new L.GeoJSON(data);
+        this.map.addLayer(featuresLayer);
+        var searchControl = new L.Control.Search({layer: featuresLayer, propertyName: 'name', circleLocation:false});
+        this.map.addControl( searchControl);
+    }    
+    
+    
 }
 
 
